569b1b61e0ccbeb6d02984b13abfdee050c5517d
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2024-06-10 18:20:17 +0500

    Naveed : Updating Verification Code

--- app/src/main/java/io/github/hmzi67/securezone/Activities/IntroScreenActivity.java
+++ app/src/main/java/io/github/hmzi67/securezone/Activities/IntroScreenActivity.java
@@ -56,7 +56,7 @@ public class IntroScreenActivity extends AppCompatActivity {
                 startActivity(mainActivity);
                 finish();
             } else {
-                Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+                Intent mainActivity = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(mainActivity);
                 finish();
             }
