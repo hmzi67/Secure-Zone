58ba58893d167187ae830c95168de6553bff734e
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2024-06-15 21:18:08 +0500

    Naveed : Updating Chat Activity

--- app/src/main/java/io/github/hmzi67/securezone/Activities/AIChatActivity.java
+++ app/src/main/java/io/github/hmzi67/securezone/Activities/AIChatActivity.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
+import io.github.uxlabspk.cloudmeeting.Models.AllMessagesModel;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import io.github.hmzi67.securezone.Adapters.MessageAdapter;
 import io.github.hmzi67.securezone.R;
 import io.github.hmzi67.securezone.databinding.ActivityAiChatBinding;
 
 public class AIChatActivity extends AppCompatActivity {
     private ActivityAiChatBinding binding;
+    private ArrayList<io.github.uxlabspk.cloudmeeting.Models.AllMessagesModel> allMessages = new ArrayList<>();
+    private MessageAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,56 @@ public class AIChatActivity extends AppCompatActivity {
         // go back
         binding.goBack.setOnClickListener(view -> onBackPressed());
 
+        // getMessages();
+
+        // setting the recycler view
+        adapter = new MessageAdapter();
+        adapter.setAllMessages(allMessages, this);
+        binding.rvUserChat.setAdapter(adapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        llm.setSmoothScrollbarEnabled(true);
+        binding.rvUserChat.setLayoutManager(llm);
+
+        // on message sent.
+        binding.sendBtn.setOnClickListener(view -> {
+            AllMessagesModel model = new AllMessagesModel(binding.messageText.getText().toString(), "mAuth.getUid()", new Date().getTime());
+            binding.messageText.setText("");
+//            mDatabase.getReference().child("chats").child(senderRoom).child("messages").push().setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                @Override
+//                public void onComplete(@NonNull Task<Void> task) {
+//                    mDatabase.getReference().child("chats")
+//                            .child(receiverRoom)
+//                            .child("messages")
+//                            .push().setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Void> task) {
+//
+//                                }
+//                            });
+//                }
+//            });
+        });
 
     }
+
+//    private void getMessages() {
+//        mDatabase.getReference().child("chats").child(senderRoom).child("messages").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (allMessages != null) allMessages.clear();
+//                for (DataSnapshot ds: snapshot.getChildren()) {
+//                    AllMessagesModel model = ds.getValue(AllMessagesModel.class);
+//                    allMessages.add(model);
+//                }
+//                adapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//    }
+
 }
--- app/src/main/java/io/github/hmzi67/securezone/Adapters/MessageAdapter.java
+++ app/src/main/java/io/github/hmzi67/securezone/Adapters/MessageAdapter.java
@@ -14,9 +14,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import io.github.hmzi67.securezone.R;
 import io.github.uxlabspk.cloudmeeting.Models.AllMessagesModel;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder>{
+    private ArrayList<AllMessagesModel> allMessages = new ArrayList<>();
     private Context context;
 
     @NonNull
@@ -29,31 +31,32 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-//        AllMessagesModel message = allMessages.get(position);
+        AllMessagesModel message = allMessages.get(position);
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
 
-//        if (message.getSentBy().matches(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
-//            holder.left_chat_view.setVisibility(View.GONE);
-//            holder.right_chat_view.setVisibility(View.VISIBLE);
-//            holder.right_chat_text.setText(message.getMessage());
-//            holder.right_chat_text_time.setText(new TimeFormatter(message.getSentTime()).formattedTime());
-//            holder.chat_read_status.setVisibility(message.isSeen() ? View.VISIBLE : View.GONE);
-//        } else {
-//            holder.right_chat_view.setVisibility(View.GONE);
-//            holder.left_chat_view.setVisibility(View.VISIBLE);
-//            holder.left_chat_text.setText(message.getMessage());
+        if (message.getSentBy().matches("M")) {
+            holder.left_chat_view.setVisibility(View.GONE);
+            holder.right_chat_view.setVisibility(View.VISIBLE);
+            holder.right_chat_text.setText(message.getMessage());
+//            holder.right_chat_text_time.setText(formatter.format(new Time(Long.parseLong(time + ""))));
+            holder.chat_read_status.setVisibility(message.isSeen() ? View.VISIBLE : View.GONE);
+        } else {
+            holder.right_chat_view.setVisibility(View.GONE);
+            holder.left_chat_view.setVisibility(View.VISIBLE);
+            holder.left_chat_text.setText(message.getMessage());
 //            holder.left_chat_text_time.setText(new TimeFormatter(message.getSentTime()).formattedTime());
-//        }
+        }
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;// allMessages.size();
+        return allMessages.size();
     }
 
     public void setAllMessages(ArrayList<AllMessagesModel> allMessages, Context context) {
-//        this.allMessages = allMessages;
-//        this.context = context;
+        this.allMessages = allMessages;
+        this.context = context;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
