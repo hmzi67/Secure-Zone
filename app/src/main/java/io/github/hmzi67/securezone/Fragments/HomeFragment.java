06447f894173edc6ae783c3a3bacb181961e629e
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2024-06-23 15:31:47 +0500

    Naveed : Adding Open Street Map

--- app/src/main/java/io/github/hmzi67/securezone/Fragments/HomeFragment.java
+++ app/src/main/java/io/github/hmzi67/securezone/Fragments/HomeFragment.java
@@ -2,14 +2,19 @@ package io.github.hmzi67.securezone.Fragments;
 
 import static androidx.core.content.ContextCompat.checkSelfPermission;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,17 +23,15 @@ import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
-import io.github.hmzi67.securezone.Activities.SettingsActivity;
 import io.github.hmzi67.securezone.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private IMapController mapController;
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
+    public HomeFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,14 +44,12 @@ public class HomeFragment extends Fragment {
     private void init() {
         Configuration.getInstance().load(getContext(), PreferenceManager.getDefaultSharedPreferences(getContext()));
 
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            if (isStoragePermissionGranted()){
-//
-//            }
-//        }
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (isStoragePermissionGranted()){
 
+            }
+        }
 
-        // map = findViewById(R.id.mapView);
         binding.mapView.setTileSource(TileSourceFactory.MAPNIK);
         binding.mapView.setBuiltInZoomControls(true);
         binding.mapView.setMultiTouchControls(true);
@@ -56,24 +57,57 @@ public class HomeFragment extends Fragment {
         mapController.setZoom(15);
         GeoPoint startPoint = new GeoPoint(51496994, -134733);
         mapController.setCenter(startPoint);
+
+        MyLocationNewOverlay myLocationOverlay = new MyLocationNewOverlay(binding.mapView);
+        myLocationOverlay.enableMyLocation();
+        binding.mapView.getOverlays().add(myLocationOverlay);
+        binding.mapView.invalidate();
     }
 
-//    private boolean isStoragePermissionGranted() {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-//                    == PackageManager.PERMISSION_GRANTED ; checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-//                    == PackageManager.PERMISSION_GRANTED) {
-//                Log.v(TAG, "Permission is granted");
-//                return true;
-//            } else {
-//
-//                Log.v(TAG, "Permission is revoked");
-//                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-//                return false;
-//            }
-//        } else { //permission is automatically granted on sdk<23 upon installation
-//            Log.v(TAG, "Permission is granted");
-//            return true;
-//        }
-//    }
+    public void onResume() {
+        super.onResume();
+        if (binding.mapView != null)
+            binding.mapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    public void onPause() {
+        super.onPause();
+        if (binding.mapView != null)
+            binding.mapView.onPause();
+    }
+
+    private boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+
+                // Request the permission
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+
+                // Request the permission
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        1);
+
+                return true;
+            } else {
+                // Permission already granted
+                // Perform your operation related to location
+
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d("TAG", "Permission: " + permissions[0] + "was " + grantResults[0]);
+            //resume tasks needing this permission
+        }
+    }
 }
