976cf1389536d54645a2747edf4f0c07a46210f2
hmzi67 <hamzawaheed057@gmail.com>
2024-07-04 16:37:43 +0500

    Hamza: Broadcast receiver code refactor

--- app/src/main/java/io/github/hmzi67/securezone/Services/ButtonBroadCastReceiver.java
+++ app/src/main/java/io/github/hmzi67/securezone/Services/ButtonBroadCastReceiver.java
@@ -13,14 +13,14 @@ public class ButtonBroadCastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction() != null && intent.getAction().equals("android.media.VOLUME_CHANGED_ACTION")) {
             int volumeType = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_TYPE", -1);
-            int volumeDirection = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_DIRECTION", -1);
+            int volumeDirection = intent.getIntExtra("android.media.EXTRA_PREV_VOLUME_STREAM_VALUE", -1);
+            int newVolume = intent.getIntExtra("android.media.EXTRA_VOLUME_STREAM_VALUE", -1);
 
             if (volumeType == AudioManager.STREAM_MUSIC) {
-                AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                if (volumeDirection == AudioManager.ADJUST_RAISE) {
+                if (newVolume > volumeDirection) {
                     // Volume up button pressed
                     Toast.makeText(context, "Volume up", Toast.LENGTH_SHORT).show();
-                } else if (volumeDirection == AudioManager.ADJUST_LOWER) {
+                } else if (newVolume < volumeDirection) {
                     // Volume down button pressed
                     Toast.makeText(context, "Volume down", Toast.LENGTH_SHORT).show();
                 }
