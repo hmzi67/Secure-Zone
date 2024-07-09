f0a90d828dd898fbd893ac4af47843cac25b68bc
Muhammad Naveed <muhammadnaveedcis@gmail.com>
2024-07-09 08:49:34 +0500

    Naveed : Refactoring Location Model

--- app/src/main/java/io/github/hmzi67/securezone/Modals/LocationModel.java
+++ app/src/main/java/io/github/hmzi67/securezone/Modals/LocationModel.java
@@ -13,4 +13,28 @@ public class LocationModel {
         this.longitude = longitude;
         this.address = address;
     }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
